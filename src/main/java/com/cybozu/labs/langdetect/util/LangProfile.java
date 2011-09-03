8929d161ca36c90e9cda14eef0cf087e434be4ef
Francois ROLAND <fr.roland@gmail.com>
2011-09-03 15:24:58 +0200

    Encapsulated fields.

--- src/main/java/com/cybozu/labs/langdetect/util/LangProfile.java
+++ src/main/java/com/cybozu/labs/langdetect/util/LangProfile.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2011 Nakatani Shuyo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * This file has been modified by François ROLAND.
+ */
+
 package com.cybozu.labs.langdetect.util;
 
 import java.io.Serializable;
@@ -15,9 +35,9 @@ public class LangProfile implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private static final int MINIMUM_FREQ = 2;
     private static final int LESS_FREQ_RATIO = 100000;
-    public String name = null;
-    public HashMap<String, Integer> freq = new HashMap<String, Integer>();
-    public int[] n_words = new int[NGram.N_GRAM];
+    private String name = null;
+    private HashMap<String, Integer> freq = new HashMap<String, Integer>();
+    private int[] nWords = new int[NGram.N_GRAM];
 
     /**
      * Constructor for JSONIC 
@@ -29,7 +49,7 @@ public class LangProfile implements Serializable {
      * @param name language name
      */
     public LangProfile(String name) {
-        this.name = name;
+        this.setName(name);
     }
     
     /**
@@ -37,14 +57,14 @@ public class LangProfile implements Serializable {
      * @param gram
      */
     public void add(String gram) {
-        if (name == null || gram == null) return;   // Illegal
+        if (getName() == null || gram == null) return;   // Illegal
         int len = gram.length();
         if (len < 1 || len > NGram.N_GRAM) return;  // Illegal
-        ++n_words[len - 1];
-        if (freq.containsKey(gram)) {
-            freq.put(gram, freq.get(gram) + 1);
+        ++getNWords()[len - 1];
+        if (getFreq().containsKey(gram)) {
+            getFreq().put(gram, getFreq().get(gram) + 1);
         } else {
-            freq.put(gram, 1);
+            getFreq().put(gram, 1);
         }
     }
 
@@ -52,17 +72,17 @@ public class LangProfile implements Serializable {
      * Eliminate below less frequency n-grams and noise Latin alphabets
      */
     public void omitLessFreq() {
-        if (name == null) return;   // Illegal
-        int threshold = n_words[0] / LESS_FREQ_RATIO;
+        if (getName() == null) return;   // Illegal
+        int threshold = getNWords()[0] / LESS_FREQ_RATIO;
         if (threshold < MINIMUM_FREQ) threshold = MINIMUM_FREQ;
         
-        Set<String> keys = freq.keySet();
+        Set<String> keys = getFreq().keySet();
         int roman = 0;
         for(Iterator<String> i = keys.iterator(); i.hasNext(); ){
             String key = i.next();
-            int count = freq.get(key);
+            int count = getFreq().get(key);
             if (count <= threshold) {
-                n_words[key.length()-1] -= count; 
+                getNWords()[key.length()-1] -= count; 
                 i.remove();
             } else {
                 if (key.matches("^[A-Za-z]$")) {
@@ -72,16 +92,40 @@ public class LangProfile implements Serializable {
         }
 
         // roman check
-        if (roman < n_words[0] / 3) {
-            Set<String> keys2 = freq.keySet();
+        if (roman < getNWords()[0] / 3) {
+            Set<String> keys2 = getFreq().keySet();
             for(Iterator<String> i = keys2.iterator(); i.hasNext(); ){
                 String key = i.next();
                 if (key.matches(".*[A-Za-z].*")) {
-                    n_words[key.length()-1] -= freq.get(key); 
+                    getNWords()[key.length()-1] -= getFreq().get(key); 
                     i.remove();
                 }
             }
             
         }
     }
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public HashMap<String, Integer> getFreq() {
+		return freq;
+	}
+
+	public void setFreq(HashMap<String, Integer> freq) {
+		this.freq = freq;
+	}
+
+	public int[] getNWords() {
+		return nWords;
+	}
+
+	public void setNWords(int[] nWords) {
+		this.nWords = nWords;
+	}
 }
